165aef6675ad61d93a7960a5975945279b67366d
terencegaffudSU <terence.gaffud@skillsunion.com>
2024-09-27 14:24:53 +0800

    Removed reference

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>
-        <img src={logo} className="logo react" alt="Rocket logo" />
-      </div>
       <h1>Weather App</h1>
       <div className="card">
-        {/* Follow the weather app instructions on the gitbook to implement this exercise */}
+        {/* Follow the weather app instructions on the exercise file */}
       </div>
     </>
   );
